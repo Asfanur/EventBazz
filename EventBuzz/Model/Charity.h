37dc7d5d8f991a8a717981cb14c4a7c035104a55
armin ahmadi <armin.ahmadi@gmail.com>
2015-02-07 15:32:22 +1100

    test2

---
+++ EventBuzz/Model/Charity.h
@@ -0,0 +1,22 @@
+//
+//  Charity.h
+//  EventBuzz
+//
+//  Created by ARMIN on 7/02/2015.
+//  Copyright (c) 2015 ARMIN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Charity : NSObject
+
+@property (nonatomic, strong) NSString *charityID;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *mission;
+@property (nonatomic) float percentage;
+
+- (id)initWithName:(NSString *)name andID:(NSString *)charID andMission:(NSString *)mission;
+
+
+- (Charity *)copyCharity;
+@end
